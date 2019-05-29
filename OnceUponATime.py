1d02a732e8e411dc1ff326cd057b5cf7cdbec630
Chirag Tripathi <34264961+MechanicsK@users.noreply.github.com>
2019-05-29 20:10:10 +0530

    Create OnceUponATime.py

---
+++ OnceUponATime.py
@@ -0,0 +1,2 @@
+#This one goes with the historical model of business. Given a history snippet, can we figure out how different our financial 
+# world could have become if it was not for what actually happened? 
